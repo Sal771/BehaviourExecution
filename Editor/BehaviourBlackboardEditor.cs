2262eb654e788a52cabb562bd5d0c8a5e961e1bf
DavideDalDosso <95852180+Sal771@users.noreply.github.com>
2026-03-23 16:37:52 +0100

    v0.2.1 Removing unused dependencies

--- Editor/BehaviourBlackboardEditor.cs
+++ Editor/BehaviourBlackboardEditor.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using com.Sal77.BehaviourExecution;
-using com.Sal77.GameCore;
 using System.IO;
 
 [CustomEditor(typeof(BehaviourBlackboard))]
--- Runtime/ExecutionCore/Behaviour/Action/BehaviourActionBuffer.cs
+++ Runtime/ExecutionCore/Behaviour/Action/BehaviourActionBuffer.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 
 namespace com.Sal77.BehaviourExecution
